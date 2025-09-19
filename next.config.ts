aa8ba465801cbdc45421a5bf58dbb4973235b436
Jae Ren <mysteryjr1994@gmail.com>
2025-09-20 00:39:42 +0800

    Add basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/jaeren-resume' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/jaeren-resume/' : '',
 };
 
 export default nextConfig;
